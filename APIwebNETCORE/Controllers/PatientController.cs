793cfed0c9c56ce53c516a08d5cc0cf469921cda
youssef98ys <youssef98.ys@gmail.com>
2022-01-11 21:03:30 +0100

    first commit

---
+++ APIwebNETCORE/Controllers/PatientController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using APIwebNETCORE.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace APIwebNETCORE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly IDocumentClient _documentClient;
+        readonly String databseId;
+        readonly String collectionId;
+        public IConfiguration Configuration { get; }
+
+        public PatientController(IDocumentClient documentClient, IConfiguration configuration)
+        {
+            _documentClient = documentClient;
+            Configuration = configuration;
+
+            databseId = Configuration["DatabaseId"];
+            collectionId = "Patients";
+            BuildCollection().Wait();
+        }
+
+        private async Task BuildCollection()
+        {
+            await _documentClient.CreateDatabaseIfNotExistsAsync(new Database { Id = databseId });
+            await _documentClient.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(databseId),
+                new DocumentCollection { Id = collectionId });
+        }
+
+        [HttpGet]
+        public IQueryable<Patient> Get()
+        {
+            return _documentClient.CreateDocumentQuery<Patient>(UriFactory.CreateDocumentCollectionUri(databseId, collectionId),new FeedOptions { MaxItemCount = 20});
+        }
+
+        [HttpGet("{id}")]
+        public IQueryable<Patient> Get(string id)
+        {
+            return _documentClient.CreateDocumentQuery<Patient>(UriFactory.CreateDocumentCollectionUri(databseId, collectionId),
+                new FeedOptions { MaxItemCount = 1 }).Where((i) => i.id == id);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Patient patient)
+        {
+            var response = await _documentClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databseId, collectionId), patient);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] Patient item)
+        {
+            await _documentClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databseId, collectionId, id),
+                item);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _documentClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databseId, collectionId, id));
+            return Ok();
+        }
+    }
+}
---
+++ APIwebNETCORE/Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿namespace APIwebNETCORE.Models
+{
+    public class Patient
+    {
+        public string Num { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Adress { get; set; }
+
+        public Boolean Malade { get; set; }
+        public int Score { get; set; }
+
+        [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+    }
+}
